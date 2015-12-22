af0141180bc32b2112c59b2e6fff03d5da1cc257
Adrian Booth <Adrian.booth17@gmail.com>
2015-12-22 22:09:18 +0000

    Not working when I create new posts controller. Confused

--- app.js
+++ app.js
@@ -5,21 +5,31 @@ angular.module('redditNews', [
   '$stateProvider',
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider){
+
     $stateProvider
       .state('home',{
         url: '/home',
         templateUrl: '/home.html',
         controller: 'MainCtrl'
-      });
+      })
+
+      .state('posts',{
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      })
+
     $urlRouterProvider.otherwise('home');
-  }
-])
+  }])
+
+
 .factory('posts', [function(){
     var o = {
       posts: []
     };
     return o;
 }])
+
 .controller('MainCtrl', [
   '$scope',
   'posts',
@@ -27,7 +37,15 @@ angular.module('redditNews', [
     $scope.posts = posts.posts;
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') {return; }
-      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+      $scope.posts.push({
+  title: $scope.title,
+  link: $scope.link,
+  upvotes: 0,
+  comments: [
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  ]
+});
       };
     $scope.incrementUpvotes = function(post){
       post.upvotes += 1;
@@ -41,3 +59,20 @@ angular.module('redditNews', [
         {title: 'post 5', upvotes: 4}
       ];
   }]);
+
+// .controller('PostsCtrl', [
+// '$scope',
+// '$stateParams',
+// 'posts',
+// $scope.post = posts.posts[$stateParams.id];
+// function($scope, $stateParams, posts){
+//   $scope.addComment = function(){
+//   if($scope.body === '') { return; }
+//   $scope.post.comments.push({
+//     body: $scope.body,
+//     author: 'user',
+//     upvotes: 0
+//   });
+//   $scope.body = '';
+// };
+// }]);
