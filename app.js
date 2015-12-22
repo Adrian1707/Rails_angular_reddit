0493d49ae7f5e99927e2dd14ea7e237dbc257483
Adrian Booth <Adrian.booth17@gmail.com>
2015-12-22 14:52:22 +0000

    Prevent user from submitting blank form

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ angular.module('redditNews', [])
   '$scope',
   function($scope){
     $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') {return; }
       $scope.posts.push({title: $scope.title, upvotes: 0});
-    };
-    $scope.test = 'Hello world!';
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
+      };
+      $scope.test = 'Hello world!';
+      $scope.posts = [
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 2},
+        {title: 'post 3', upvotes: 15},
+        {title: 'post 4', upvotes: 9},
+        {title: 'post 5', upvotes: 4}
+      ];
   }]);
